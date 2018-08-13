4a051a43ab30f36bcbc751bec3ba3d9d653cfb6b
Jose Menes <jmenes1729@gmail.com>
2018-08-13 16:42:49 -0400

    Initial commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const DashboardPage = () => (
+    <div>
+        DashboardPage content
+    </div>
+);
+
+export default DashboardPage;
